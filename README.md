e72ea7b7b869bfc45f57518d27bc581da3ca9933
youngkimi <kimyoungseop33@gmail.com>
2025-07-28 15:39:02 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# exec-spring-outbox-sheduling
---
+++ src/main/java/io/outbox/OutboxApplication.java
@@ -0,0 +1,13 @@
+package io.outbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutboxApplication.class, args);
+	}
+
+}
